9d0dca23fdf91465f2e972e466dc51bf4c121b2a
nini <77511500+niniiturner@users.noreply.github.com>
2025-06-12 20:25:03 -0600

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './slack/slackService';
+export * from './slack/slackConfig';
+export * from './jira/jiraService';
+export * from './jira/jiraConfig';
+export * from './gitlab/gitlabService';
+export * from './gitlab/gitlabConfig';
